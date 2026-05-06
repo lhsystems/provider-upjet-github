8541002e5cd3c08d19ac7e401c5a7a325c583c26
benfu99 <benfu99@yahoo.de>
2026-05-06 10:43:59 +0200

    feat: add generated files for HostedRunner resource

--- apis/cluster/actions/v1alpha1/zz_generated.managed.go
+++ apis/cluster/actions/v1alpha1/zz_generated.managed.go
@@ -208,6 +208,56 @@ func (mg *EnvironmentVariable) SetWriteConnectionSecretToReference(r *xpv1.Secre
 	mg.Spec.WriteConnectionSecretToReference = r
 }
 
+// GetCondition of this HostedRunner.
+func (mg *HostedRunner) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this HostedRunner.
+func (mg *HostedRunner) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetManagementPolicies of this HostedRunner.
+func (mg *HostedRunner) GetManagementPolicies() xpv1.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this HostedRunner.
+func (mg *HostedRunner) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetWriteConnectionSecretToReference of this HostedRunner.
+func (mg *HostedRunner) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this HostedRunner.
+func (mg *HostedRunner) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this HostedRunner.
+func (mg *HostedRunner) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetManagementPolicies of this HostedRunner.
+func (mg *HostedRunner) SetManagementPolicies(r xpv1.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this HostedRunner.
+func (mg *HostedRunner) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetWriteConnectionSecretToReference of this HostedRunner.
+func (mg *HostedRunner) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this OrganizationActionsSecret.
 func (mg *OrganizationActionsSecret) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)
