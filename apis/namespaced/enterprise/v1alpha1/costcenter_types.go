1492761a768b63d422e54d3de9c52f1f9a3ae01e
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2026-09-24 12:11:20 +0200

    Update CostCenterSpec to use ManagedResourceSpec

--- apis/namespaced/enterprise/v1alpha1/costcenter_types.go
+++ apis/namespaced/enterprise/v1alpha1/costcenter_types.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	v1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+	v2 "github.com/crossplane/crossplane-runtime/v2/apis/common/v2"
 )
 
 type CostCenterResource struct {
@@ -38,8 +39,8 @@ type CostCenterParameters struct {
 }
 
 type CostCenterSpec struct {
-	v1.ResourceSpec `json:",inline"`
-	ForProvider     CostCenterParameters `json:"forProvider"`
+	v2.ManagedResourceSpec `json:",inline"`
+	ForProvider            CostCenterParameters `json:"forProvider"`
 }
 
 type CostCenterStatus struct {
